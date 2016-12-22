1317cd57512782c95024269d6ac6a83e1f787a59
liu <1020269358@qq.com>
2016-12-22 18:40:10 +0800

    Tetris

--- js/animation.js
+++ js/animation.js
@@ -2,25 +2,25 @@ function drawBackGround(){
     var canvas = document.getElementById("canvas-bg");
     var context = canvas.getContext("2d");
 
-<<<<<<< HEAD
+// <<<<<<< HEAD
     canvas.width = 450;
     canvas.height = 720;
 
     context.fillStyle = '#666';
     context.fillRect(0, 120, 450, 600);
-=======
+// =======
     canvas.width = 300;
     canvas.height = 480;
 
     context.fillStyle = '#666';
     context.fillRect(0, 80, 300, 400);
->>>>>>> gh-pages
+// >>>>>>> gh-pages
 }
 
 function drawNextBlockBG(){
     var canvas_next = document.getElementById("next-bg");
     var context_next = canvas_next.getContext("2d");
-<<<<<<< HEAD
+// <<<<<<< HEAD
     canvas_next.width = 300;
     canvas_next.height = 180;
     context_next.fillStyle = '#242154';
@@ -30,7 +30,7 @@ function drawNextBlockBG(){
 function updateBG(bg){
     var canvasWidth = 450;
     var canvasHeight = 720;
-=======
+// =======
     canvas_next.width = 200;
     canvas_next.height = 120;
     context_next.fillStyle = '#242154';
@@ -40,7 +40,7 @@ function updateBG(bg){
 function updateBG(bg){
     var canvasWidth = 300;
     var canvasHeight = 480;
->>>>>>> gh-pages
+// >>>>>>> gh-pages
     var canvas = document.getElementById('canvas-block');
     var context = canvas.getContext('2d');
     canvas.width = canvasWidth;
@@ -54,15 +54,15 @@ function updateBG(bg){
         if(i > 3){
             for(j in row){
                 if(row[j] === 1){
-<<<<<<< HEAD
+// <<<<<<< HEAD
                     start_y = i*30;
                     start_x = j*30;
                     context.fillRect(start_x, start_y, 30, 30);
-=======
+// =======
                     start_y = i*20;
                     start_x = j*20;
                     context.fillRect(start_x, start_y, 20, 20);
->>>>>>> gh-pages
+// >>>>>>> gh-pages
                 }
             }
         }       
@@ -77,14 +77,14 @@ function drawNextBlock(data){
     var y;
     var x;
     for(YX of data){
-<<<<<<< HEAD
+// <<<<<<< HEAD
         y = YX[0] * 30;
         x = YX[1] * 30;
         context_next.fillRect(x, y, 30, 30);
-=======
+// =======
         y = YX[0] * 20;
         x = YX[1] * 20;
         context_next.fillRect(x, y, 20, 20);
->>>>>>> gh-pages
+// >>>>>>> gh-pages
     }
 }
